2a1f5b6c0ce00432f9bc76aae041067e6bfe11ce
Marcoglg2002 <marcoantoniorodrigues200201@gmail.com>
2022-05-07 14:33:26 -0300

    ok

--- Ex 02- Maquina de saque.c
+++ Ex 02- Maquina de saque.c
@@ -1,70 +1,86 @@
 #include <stdio.h>
 
-// Por: Marco Antonio Rodrigues Gomes de Souza
-// Esse codigo simula uma maquina de sacar dinheiro, na qual ele
-// recebe uma entrada do valor que o usuario quer sacar e tem como saida o numero de cedulas
-// que foram sacadas e se sobrar algo ele informar ao usuario.
-int main(){
+int main()
+{
 
-    // variaveis
-    int numerodigitado, nota100, nota50, nota20, nota10, nota5, nota2;
-    nota100 =0;
-    nota50 =0;
-    nota20 =0;
-    nota10 =0;
-    nota5 =0;
-    nota2 =0;
+    int valor, s, c100, c50, c20, c10, c5, c2, c1;
 
-    // entrada de dados
     printf("digite um valor para saque(notas disponiveis 100,50,20,10,5,2): ");
-    scanf("%i", &numerodigitado);
+    scanf("%i", &valor);
 
-    // algoritmo
-    if(numerodigitado >= 100){
-        nota100 = numerodigitado/100;
-        numerodigitado = numerodigitado % 100;
-        printf("\nnotas de R$100,00: %i",nota100);
+    while (valor == 0)
+    {
+        printf("O valor nao pode ser zero!");
+        break;
     }
-    if(numerodigitado >= 50){
-        nota50 = numerodigitado/50;
-        numerodigitado = numerodigitado % 50;
-        printf("\nnotas de R$50,00:  %i",nota50);
+
+    s = 100;
+    c100 = 0;
+    while ((valor >= 100) && (s <= valor))
+    {
+        s = s + 100;
+        c100 = c100 + 1;
     }
-    if(numerodigitado >= 20){
-        nota20 = numerodigitado/20;
-        numerodigitado = numerodigitado % 20;
-        printf("\nnotas de R$20,00:  %i",nota20);
+    valor = valor - (100 * c100);
+    printf("\n%i notas de 100\n", c100);
+
+    s = 50;
+    c50 = 0;
+    while ((valor >= 50) && (s <= valor) && (valor != 51))
+    {
+        s = s + 50;
+        c50 = c50 + 1;
     }
-    if(numerodigitado >= 10){
-        nota10 = numerodigitado/10;
-        numerodigitado = numerodigitado % 10;
-        printf("\nnotas de R$10,00:  %i",nota10);
+    valor = valor - (50 * c50);
+    printf("%i notas de 50\n", c50);
+
+    s = 20;
+    c20 = 0;
+    while ((valor >= 20) && (s <= valor) && (valor != 21))
+    {
+        s = s + 20;
+        c20 = c20 + 1;
     }
-    if(numerodigitado >= 5){
-        nota5 = numerodigitado/5;
-        numerodigitado = numerodigitado % 5;
-        printf("\nnotas de R$5,00:   %i",nota5);
+    valor = valor - (20 * c20);
+    printf("%d notas de 20\n", c20);
+
+    s = 10;
+    c10 = 0;
+    while ((10 <= valor) && (valor != 13) && (s <= valor) && (valor != 11) && (valor != 21))
+    {
+        s = s + 10;
+        c10 = c10 + 1;
     }
-    if(numerodigitado >= 2){
-        nota2 = numerodigitado/2;
-        numerodigitado = numerodigitado % 2;
-        printf("\nnotas de R$2,00:   %i",nota2);
+    valor = valor - (10 * c10);
+    printf("%d notas de 10\n", c10);
+
+    s = 5;
+    c5 = 0;
+    while ((valor >= 5) && (c5 != 1) && (valor != 6) && (valor != 8))
+    {
+        s = s + 5;
+        c5 = c5 + 1;
     }
-    if(numerodigitado >= 1){
-        printf("\n por falta de cedulas compativeis faltou R$%i,00 para o saque.",numerodigitado);
+    valor = valor - (5 * c5);
+    printf("%d notas de 5\n", c5);
+
+    s = 2;
+    c2 = 0;
+    while ((valor >= 2) && (s <= valor))
+    {
+        s = s + 2;
+        c2 = c2 + 1;
+    }
+    valor = valor - (2 * c2);
+    printf("%i notas de 2\n", c2);
+
+    while (valor == 1)
+    {
+        c1 = 0;
+        printf("\nNao sera possivel o saque de 1 real por conta da falta de cedulas, o restante sera sacado.");
+        c1 = c1 + 1;
+        printf("\nrestante na conta: R$ %i,00\n", c1);
+        break;
     }
     return 0;
 }
-
-// exemplo de execucao:
-
-// digite um valor para saque(notas disponiveis 100,50,20,10,5,2): 187
-
-// notas de R$100,00: 1
-// notas de R$50,00:  1
-// notas de R$20,00:  1
-// notas de R$10,00:  1
-// notas de R$5,00:   1
-// notas de R$2,00:   1
-
-
