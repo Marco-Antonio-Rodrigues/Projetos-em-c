5ec7ec072364081c4f7975987ef10b06d00ae996
Marco Antonio Rodrigues Gomes de Souza <94053824+Marco-Antonio-Rodrigues@users.noreply.github.com>
2023-05-04 19:17:14 -0300

    add new folder EDA

---
+++ Estruturas-de-dados-avancada/heap.c
@@ -0,0 +1,6 @@
+#include "heap.h"
+
+int main(){
+    printf("Digite a: %i ",opa);
+    return 0;
+}
---
+++ Estruturas-de-dados-avancada/heap.h
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#ifndef TR89_511354
+#define TR89_511354
+
+int opa = 0;
+int teste();
+
+#endif
+
+
